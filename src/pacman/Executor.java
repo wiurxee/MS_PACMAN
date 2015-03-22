36bef2fd094ad7c2117d786f2864440d6c023f85
Aram <aram_rodriguez@msn.com>
2015-03-22 22:07:33 +0100

    Ataque (falta comentar)

--- src/pacman/Executor.java
+++ src/pacman/Executor.java
@@ -50,10 +50,10 @@ public class Executor
 
 		
 		//run multiple games in batch mode - good for testing.
-		int numTrials=50;
-		exec.runExperiment(new AJICONTROLLER(),new StarterGhosts(),numTrials);
+		int numTrials=500;
+		//exec.runExperiment(new AJICONTROLLER(),new StarterGhosts(),numTrials);
 		exec.replayGame("maxScoreReplay", true);
-		exec.replayGame("minScoreReplay", true);
+		//exec.replayGame("minScoreReplay", true);
 		
 //		
 //		//run a game in synchronous mode: game waits until controllers respond.
